cf9e97da5b221c011796fb011f2e4b967c6b91bb
BraLaryea <laryea.enl99@gmail.com>
2022-04-08 15:53:09 +0000

    update

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const blogRoutes = require('./routes/blogRoutes')
 
 //connect to mongoDB
 const dbURI = 'mongodb+srv://bralaryea:0909@nodetuts.uowrp.mongodb.net/node-tuts?retryWrites=true&w=majority';
+// const dbURI = 'mongodb://bralaryea:0909@_mongodb._tcp.nodetuts.uowrp.mongodb.net/node-tuts?ssl=true&replicaSet=atlas-5h268a-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(5000))
@@ -14,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('assets'))
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {res.redirect('/blogs')})
+app.get('/', (req, res) => { res.redirect('/blogs') })
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
